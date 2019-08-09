7d9775ed0d7527e0ddd9991731d4a008fad0ec87
cmyui <cmyuisuki@gmail.com>
2019-08-09 13:57:03 -0400

    Fat rewrite

--- parser.py
+++ parser.py
@@ -1,166 +1,229 @@
-def remove_replay_headers(replayFile, debug=False):
-    """
-    A function used to remove a replay's headers, and take just the LZMA data.
+"""
+from enum import Enum
 
-    :param replayFile: The directory to the replay file.
-    :return 1: Failed to remove replay headers.
-    :return 0: Successfully removed replay headers.
-    """
-    import lzma, struct
 
-    # Constant data types.
+class GameMode(Enum):
+    osu       = 0
+    osu_taiko = 1
+    osu_catch = 2
+    osu_mania = 3
+
+
+class Mod(Enum):
+    NoMod          = 0
+    NoFail         = 1
+    Easy           = 2
+    NoVideo        = 4
+    Hidden         = 8
+    HardRock       = 16
+    SuddenDeath    = 32
+    DoubleTime     = 64
+    Relax          = 128
+    HalfTime       = 256
+    Nightcore      = 512
+    Flashlight     = 1024
+    Autoplay       = 2048
+    SpunOut        = 4096
+    Autopilot      = 8192
+    Perfect        = 16384
+    Key4           = 32768
+    Key5           = 65536
+    Key6           = 131072
+    Key7           = 262144
+    Key8           = 524288
+    keyMod         = 1015808
+    FadeIn         = 1048576
+    Random         = 2097152
+    LastMod        = 4194304
+    TargetPractice = 8388608
+    Key9           = 16777216
+    Coop           = 33554432
+    Key1           = 67108864
+    Key3           = 134217728
+    Key2           = 268435456
+
+
+class ReplayAction(object):
+    ###
+    Defines a replay action event.
+
+    :param w: Time in milliseconds since the previous action.
+    :param x: x-coordinate of the cursor from 0 - 512.
+    :param y: y-coordinate of the cursor from 0 - 384.
+    :param z: Bitwise combination of keys/mouse buttons pressed.
+              (
+                M1 = 1,
+                M2 = 2,
+                K1 = 4,
+                K2 = 8,
+                Smoke = 16
+              )
+              (
+                K1 is always used with M1;
+                K2 is always used with M2:
+                1+4=5;
+                2+8=10
+              )
+    ###
+    # Note: On replays set on version 20130319 or later,
+    # the 32-bit integer RNG seed used for the score will be encoded into an additional replay frame at the end of the LZMA stream,
+    # under the format -12345|0|0|seed.
+    def __init__(self, w, x, y, z):
+        self.time_since_previous_action = w
+        self.x = x
+        self.y = y
+        self.keys_pressed = z
+
+    def edit_action(self, w=None, x=None, y=None, z=None):
+        if w: self.time_since_previous_action = w
+        if x: self.x = x
+        if y: self.y = y
+        if z: self.keys_pressed = z
+"""
+
+class Replay(object):
     __BYTE  = 1
     __SHORT = 2
     __INT   = 4
     __LONG  = 8
 
-    # Variable types.
-    offset  = 0
+    def __init__(self, replay_data, replay_file):
+        self.compressed_data = replay_data
+        self.replay_file = "NH - [" + str(replay_file.split('\\')[-1].split('.osr')[0]) + "].osr"
+        self.decompressed = None
+        self.offset = 0
 
-    with open(replayFile, "rb") as replay_with_headers:
-        data = replay_with_headers.read()
-        #data = replay_with_headers.read()[:-8] # ScoreID is stored at the end of the of replay file, as a long.
+        self.gamemode = None
+        self.osu_version = None
+        self.beatmap_md5 = None
+        self.username = None
+        self.osu_replay_md5 = None
 
-    game_mode = data[offset]
-    offset += __BYTE
+        self.num_300s = 0
+        self.num_100s = 0
+        self.num_50s = 0
+        self.num_gekis = 0
+        self.num_katus = 0
+        self.num_misses = 0
+        self.total_score = 0
+        self.max_combo = 0
+        self.full_combo = False
+        self.mods = 0
 
-    #if Gamemode > 4 or Gamemode < 0: return 1 # Broke gamemode?
+        self.hp_graph_data = []
 
-    osu_version = struct.unpack('<l', data[offset:offset + __INT])[0]; offset += __INT
+        self.timestamp = 0
+        self.sizeof_lzma = 0
 
-    _exists = data[offset]; offset += __BYTE
+        self.online_score_id = 0
 
-    if _exists == 11: # Beatmap MD5 exists. Process it then add to offset.
-        offset_end = offset + data[offset] + __BYTE
-        offset += __BYTE
+        self.initialize_fields()
 
-        beatmap_md5 = data[offset:offset_end].decode('utf-8', 'ignore')
-        if debug: print(f"Beatmap MD5: {beatmap_md5}")
-        offset = offset_end
-        del offset_end
-    elif not _exists:
-        pass
-    else:
-        return 1
-    del _exists
 
-    _exists = data[offset]
-    offset += __BYTE
-    if _exists == 11: # Username
-        offset_end = offset + data[offset] + __BYTE
-        offset += __BYTE
+    def initialize_fields(self):
+        from time import time
 
-        username = data[offset:offset_end].decode('utf-8', 'ignore') # Set the username
-        if debug: print(f"Username: {username}")
+        self.parse_main_variables() # gamemode & osu_version
+        self.parse_main_strings() # beatmap_md5 - osu_replay_md5
+        self.parse_score_variables() # num300 - mods
+        self.parse_secondary_variables() # HP
 
-        offset = offset_end
-        del offset_end
-    elif not _exists:
-        pass
-    else:
-        return 1
-    del _exists
+        self.parse_lzma_replay() # Actual replay data
 
-    _exists = data[offset]
-    offset += __BYTE
-    if _exists == 11: # osu! replay MD5 hash.
-        offset_end = offset + data[offset] + __BYTE; offset += __BYTE
+    def parse_main_variables(self):
+        import struct
 
-        osu_replay_md5 = data[offset:offset_end].decode('utf-8', 'ignore')
-        offset = offset_end
-        del offset_end
-    elif not _exists:
-        pass
-    else:
-        return 1
-    del _exists
+        # Set gamemode.
+        _gamemode = self.compressed_data[self.offset]
+        if _gamemode < 0 or _gamemode > 3: raise Exception(f"Invalid gamemode {_gamemode}.")
+        self.gamemode = _gamemode
+        self.offset += self.__BYTE
 
-    # Unpack a lot of 2/4 byte vars.
-    num300s   = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    num100s   = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    num50s    = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    numGekis  = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    numKatus  = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    numMisses = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    TotalScore = struct.unpack('<L', data[offset:offset + __INT])[0]; offset += __INT
-    GreatestCombo = struct.unpack('<H', data[offset:offset + __SHORT])[0]; offset += __SHORT
-    FullCombo = bool(data[offset]); offset += __BYTE
-    Mods = struct.unpack('<L', data[offset:offset + __INT])[0]; offset += __INT
+        # Unpack osu version.
+        _osu_version = struct.unpack('<l', self.compressed_data[self.offset:self.offset + self.__INT])[0]
+        if type(_osu_version) is not int: raise Exception(f"Invalid osu! version {_osu_version}.")
+        self.osu_version = _osu_version
+        self.offset += self.__INT
 
-    # Debug print.
-    if debug: print(f"300s: {num300s}\n100s: {num100s}\n50s: {num50s}\nGekis: {numGekis}\nKatus: {numKatus}\nMisses: {numMisses}\nTotal score: {TotalScore}\nGreatest combo: {GreatestCombo}\nFC: {FullCombo}\nMods:{Mods}")
 
-    #TODO: stuck lol
-    #print(f"\nWHERE THE FUCK ARE WE CHECK!\n\n{data[offset-20:offset+20]}\n\n")
-    #for why in data[offset:offset+100]:print(why)
-    #return
+    def parse_main_strings(self):
+        import struct
 
-    _exists = data[offset]; offset += __BYTE
+        self.beatmap_md5 = self.parse_string()
+        self.username = self.parse_string()
+        self.osu_replay_md5 = self.parse_string()
 
-    print(f"HP BAR EXISTENCE???? : {_exists}")
-    if _exists == 11: # Life bar graph
-        print(f"STEP 2??? {data[offset]}")
-        # this one is tricky
-        offset_end = offset + data[offset] + __BYTE; offset += __BYTE
-        print(data[offset])
 
-        _HP = data[offset:offset_end].decode('utf-8', 'ignore')
-        print(f"HP: {_HP}")
-        offset = offset_end
-        del offset_end
-    elif not _exists:
-        pass
-    else:
-        return 1
-    del _exists
-    print(f"F OFFSET: {offset}")
+    def parse_score_variables(self):
+        import struct
 
-    timestamp = struct.unpack('<Q', data[offset:offset + __LONG])[0]; offset += __LONG
-    if debug: print(f"time_stamp: {timestamp}")
+        # TODO: unsafe?
+        self.num_300s = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.num_100s = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.num_50s = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.num_gekis = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.num_katus = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.num_misses = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.total_score = struct.unpack('<l', self.compressed_data[self.offset:self.offset + self.__INT])[0]; self.offset += self.__INT
+        self.max_combo = struct.unpack('<h', self.compressed_data[self.offset:self.offset + self.__SHORT])[0]; self.offset += self.__SHORT
+        self.full_combo = self.compressed_data[self.offset]; self.offset += self.__BYTE
+        self.mods = struct.unpack('<l', self.compressed_data[self.offset:self.offset + self.__INT])[0]; self.offset += self.__INT
 
-    # TODO: this im eager to moveon to re[lay data yeah]
-    replay_data_length = struct.unpack('<L', data[offset:offset + __INT])[0];offset += __INT
-    if debug: print(f"sizeof_lzma: {replay_data_length}")
 
-    online_score_id = struct.unpack('<Q', data[-8:])[0];#offset += __LONG
-    if debug: print(f"online_score_id: {online_score_id}")
+    def parse_secondary_variables(self):
+        import struct
 
-    if replay_data_length != len(data[offset:-8]): return 1 # Fuck
+        _hp_graph_data = self.parse_string()
+        if _hp_graph_data:
+            for _ in self.parse_string().split("|"): self.hp_graph_data.append(_) # Cursed line? also definitely improvable with some [for] magic
 
-    decompressed = lzma.decompress(data[offset:-8])
+        self.timestamp = struct.unpack('<q', self.compressed_data[self.offset:self.offset + self.__LONG])[0]; self.offset += self.__LONG
+        self.sizeof_lzma = struct.unpack('<l', self.compressed_data[self.offset:self.offset + self.__INT])[0]; self.offset += self.__INT
 
-    """
-    #print(data)
 
-    print(f"Using offset: {offset}")
+    def parse_lzma_replay(self):
+        import struct, lzma
 
-    for i in range(offset, len(data) // 5): # I dont wanna learn uleb rn im too high
-        try: decompressed = lzma.decompress(data[i:])
-        except lzma.LZMAError: continue
-        break
-    """
+        #if self.sizeof_lzma != len(self.compressed_data[self.offset:-8]): return 1 # Fuck
 
-    if not decompressed: return 1
+        self.decompressed = lzma.decompress(self.compressed_data[self.offset:-8], format=lzma.FORMAT_ALONE)
 
-    # TODO: check replay name for scoreid.
-    # if scoreid just overwrite the old file to keep the name.
-    with open("NH - [{}].osr".format(replayFile.split('\\')[-1]), "wb") as replay_without_headers:
-        replay_without_headers.write(lzma.compress(decompressed))
+        self.online_score_id = struct.unpack('<q', self.compressed_data[-8:])[0]
 
-    return 0
+
+    def parse_string(self):
+        exists = False
+        if not bytes([self.compressed_data[self.offset]]) == b'\x0b': exists = True
+        self.offset += self.__BYTE
+        if exists: return
+        offset_end = self.offset + self.compressed_data[self.offset] + self.__BYTE; self.offset += self.__BYTE
+
+        val = self.compressed_data[self.offset:offset_end].decode("utf-8", "ignore")
+        self.offset = offset_end
+        return val
+
+    def save_replay_headerless(self):
+        import lzma
+        with open(self.replay_file, "wb+") as f:
+            f.write(lzma.compress(self.decompressed, format=lzma.FORMAT_ALONE)) # SUBOPTIMAL AS FUCK! we don't need to re-lzma.. this is literally making the program x10 slower
 
 if __name__ == "__main__":
-    """
-    If we're running our program directly, let's take argv[1:] (all params passed by user after calling script).
-    This will allow the user to drag files into replay.py, and let it iterate through all params given.
-    """
     import sys
     from time import time
 
+    debug = False
+
     for replay in sys.argv[1:]:
         start_time = time()
+        with open(replay, "rb") as f:
+            r = Replay(f.read(), replay)
 
-        if remove_replay_headers(replay, True): print("Failed - " + str(replay.split('\\')[-1]))
-        else: print(f"Success - {int((time() - start_time) * 1000)}ms")
+        # getch?
+        end_time = time()
 
-    input("Press enter to exit..")
+        _st = time()
+        if input("Would you like to save the replay headerless?\n>> ").startswith('y'):
+            _st = time(); r.save_replay_headerless(); _et = time() # Timing yeehaw
+        if debug: print(f"{r.__dict__}\n\n")
+
+        print('%.2fms' % round((end_time - start_time + (_et - _st)) * 1000, 2))
